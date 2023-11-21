3b5da2585025458722e903fb04719d4a7627a765
Mister Souza <mistersouza@icloud.com>
2023-11-21 17:53:23 +0100

    get web app fully working

--- assets/scripts/simon.js
+++ assets/scripts/simon.js
@@ -1,8 +1,70 @@
 let game = {
     score: 0,
+    turn: 0,
     currentGame: [],
     moves: [],
     choices: ['button1', 'button2', 'button3', 'button4'],
 }
 
-module.exports = { game }
+const setScore = () => {
+    document.querySelector('#score').textContent = game.score;
+}
+
+const PlayAgain = () => {
+    game.score = 0;
+    game.currentGame = [];
+    game.moves = [];
+    document.querySelectorAll('.circle').forEach(button => {
+        if (button.getAttribute('data-listener') === 'false') {
+            button.addEventListener('click', event => {
+                let move = event.target.getAttribute('id');
+                lightUp(move);
+                game.moves.push(move);
+                handleMoveAndScore();
+            });
+            button.setAttribute('data-listener', true);
+        }
+    });
+    setScore();
+    botChoice();
+}
+
+const botChoice = () => {
+    game.moves = [];
+    game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
+    showMoves();
+}
+
+const lightUp = (circle) => {
+    document.querySelector(`#${circle}`).classList.add('light');
+    setTimeout(() => {
+        document.querySelector(`#${circle}`).classList.remove('light');
+    }, 400);
+}
+
+const showMoves = () => {
+    game.turn = 0;
+    let turns = setInterval(() => {
+        lightUp(game.currentGame[game.turn]);
+        game.turn++;
+        if (game.turn >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+const handleMoveAndScore = () => {
+    let index = game.moves.length - 1;
+    if (game.moves[index] === game.currentGame[index]) {
+        if (game.moves.length === game.currentGame.length) {
+            game.score++;
+            setScore();
+            botChoice();
+        }
+    } else {
+        alert('Wrong move!');
+        PlayAgain();
+    }
+}
+
+module.exports = { game, PlayAgain, setScore, botChoice, lightUp, showMoves, handleMoveAndScore }
--- assets/scripts/simon.test.js
+++ assets/scripts/simon.test.js
@@ -1,8 +1,10 @@
-const { game } = require('../../assets/scripts/simon.js');
+const { game, PlayAgain, setScore, botChoice, lightUp, showMoves, handleMoveAndScore } = require('../../assets/scripts/simon.js');
+
+jest.spyOn(window, 'alert').mockImplementation(() => {});
 
 beforeAll(() => {
     let fs = require('fs');
-    let fileContents = fs.readFileSync('/home/user/Desktop/simon/index.html', 'utf8');
+    let fileContents = fs.readFileSync('index.html', 'utf8');
     document.open();
     document.write(fileContents);
     document.close();
@@ -13,24 +15,35 @@ describe('Game Object', () => {
         expect(game).toBeDefined();
     });
 
-    describe('Check Game Object Properties', () => {
+    describe('Game Object Properties', () => {
         describe('Check on score property', () => {
             it('should have a property called score', () => {
-            expect(game.score).toBeDefined();
+                expect(game.score).toBeDefined();
             });
             it('should have a property called score that is a number', () => {
-            expect(typeof game.score).toBe('number');
+                expect(typeof game.score).toBe('number');
             });
             it('should have a property called score that is equal to 0', () => {
-            expect(game.score).toBe(0);
+                expect(game.score).toBe(0);
+            });
+        });
+        describe('Check on turn property', () => {
+            it('should have a property called turn', () => {
+                expect(game.turn).toBeDefined();
+            });
+            it('should have a property called turn that is a number', () => {
+                expect(typeof game.turn).toBe('number');
+            });
+            it('should have a property called turn that is equal to 0', () => {
+                expect(game.turn).toBe(0);
             });
         });
         describe('Check on currentGame property', () => {
             it('should have a property called currentGame', () => {
-            expect(game.currentGame).toBeDefined();
+                expect(game.currentGame).toBeDefined();
             });
             it('should have a property called currentGame that is an empty array', () => {
-            expect(game.currentGame).toEqual([]);
+                expect(game.currentGame).toEqual([]);
             });
         });
         describe('Check on moves property', () => {
@@ -54,6 +67,106 @@ describe('Game Object', () => {
             it('should have a property called choices that contains correct ids', () => {
                 expect(game.choices).toEqual(['button1', 'button2', 'button3', 'button4']);
             });
+        });
+    });
+});
+
+describe('setScore function', () => {
+    beforeAll(() => {
+        game.score = 42;
+        setScore();
+    });
+    it('should have a function called setScore', () => {
+        expect(setScore).toBeDefined();
+    });
+    it('should set the score display to 42', () => {
+        expect(document.querySelector('#score').textContent).toBe('42');
+    });
+});
+
+describe('playAgain function', () => {
+    beforeAll(() => {
+        game.score = 42
+        game.currentGame = ['button1', 'button2', 'button3', 'button4', 'button1', 'button2', 'button3', 'button4']
+        game.moves = ['button1', 'button2', 'button3', 'button4', 'button1', 'button2', 'button3', 'button4'];
+        document.querySelector('#score').textContent = '42';
+        PlayAgain();
+    });
+
+    it('should have a function called PlayAgain', () => {
+        expect(PlayAgain).toBeDefined();
+    });
+    it('should set game score to zero', () => {
+        expect(game.score).toBe(0);
+    });
+    it('should find a single move in currentGame array', () => {
+        expect(game.currentGame.length).toBe(1);
+    });
+    it('should empty the moves array', () => {
+        expect(game.moves).toEqual([]);
+    });
+    it('should set the score display to zero', () => {
+        expect(document.querySelector('#score').textContent).toBe('0');
+    });
+
+    describe('Test event listeners', () => {
+        it('should add event listeners to the buttons', () => {
+            let buttons = document.querySelectorAll('.circle');
+            buttons.forEach(button => {
+                expect(button.getAttribute('data-listener')).toBe('true');
+            });
+        });
+    });
+});
+
+describe('Game play', () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.moves = [];
+        botChoice();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.moves = [];
+    });
+
+    describe('Calls botChoice', () => {
+        it('should add a new move to the game.currentGame array', () => {
+            botChoice();
+            expect(game.currentGame.length).toBe(2);
+        });
+        it('should add the light class to the buttons', () => {
+            let button = document.querySelector(`#${game.currentGame[0]}`);
+            lightUp(game.currentGame[0]);
+            expect(button.classList).toContain('light');
+        });
+    });
+
+    describe('Calls showMoves', () => {
+        beforeEach(() => {
+            game.score = 42;
+        });
+        it('should update game.turnNumber', () => {
+            showMoves();
+            expect(game.turn).toBe(0);
+        });
+    });
+
+    describe('handleMoveAndScore function', () => {
+        it('should have a function called handleMoveAndScore', () => {
+            expect(handleMoveAndScore).toBeDefined();
+        });
+        it('should increment the game.score by 1', () => {
+            game.moves.push(game.currentGame[0]);
+            handleMoveAndScore();
+            expect(game.score).toBe(1);
+        });
+        it ('should call an alert if the move is wrong', () => {
+            game.moves.push('wrong');
+            handleMoveAndScore();
+            expect(window.alert).toHaveBeenCalledWith('Wrong move!');
         });
     });
 });
